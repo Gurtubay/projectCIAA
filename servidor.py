a1123b5956d022ca488b8cedfce15530948b476e
EC2 Default User <ec2-user@ip-172-31-52-39.ec2.internal>
2023-08-31 23:28:38 +0000

    Segundo commit

--- servidor.py
+++ servidor.py
@@ -1,11 +1,22 @@
 from flask import Flask, render_template
+import numpy as np
+from joblib import load
+import os
+
+#Cargar el modelo
+dt=load('dt1.joblib')
 
 #Generar el servidor(Back-End)
 servidorWeb=Flask(__name__)
 
 @servidorWeb.route("/holamundo",methods=['GET'])
-def holamundo():
+def formulario():
 	return render_template('pagina1.html')
+
+#Envio de datos a traves de JSON
+@servidorWeb.route('/modelo',methods=['POST']
+def modeloPrediccion():
+	
 
 if __name__ == '__main__':
 	servidorWeb.run(debug=False,host='0.0.0.0',port='8080')
