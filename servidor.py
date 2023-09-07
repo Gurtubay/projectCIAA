6252a1d69a70567a70ec30923a7f87d9f5e095d1
José Miguel García Gurtubay Moreno <miguelgarcia@ezigna.com>
2023-09-06 19:03:02 -0600

    Conflictos resueltos

--- servidor.py
+++ servidor.py
@@ -1,45 +1,31 @@
-from flask import Flask, render_template,jsonify,request
+from flask import Flask, render_template,request,jsonify
 import numpy as np
 from joblib import load
 import os
-from flask_login import LoginManager, UserMixin, login_user, login_required, logout_user, current_user
-#Generar el servidor(Back-End)
-servidorWeb=Flask(__name__)
-
-# Configura Flask-Login
-login_manager = LoginManager()
-login_manager.init_app(servidorWeb)
-
-# Clase de usuario (puedes personalizarla según tus necesidades)
-class User(UserMixin):
-    def __init__(self, id):
-        self.id = id
-        # Agrega cualquier otro atributo de usuario que necesites, como nombre de usuario, contraseña, etc.
-
-# Función para cargar un usuario desde la sesión
-@login_manager.user_loader
-def load_user(user_id):
-    return User(user_id)
 
 #Cargar el modelo
-dt=load('dt1.joblib')
+dt=load('dt_1.joblib')
 
-#Servidor (backend)
-servidorWeb = Flask(__name__)
+#Generar el servidor(Back-End)
+servidorWeb=Flask(_name_)
 
 @servidorWeb.route("/holamundo",methods=['GET'])
 def formulario():
-    return render_template('pagina1.html')
+	return render_template('pagina1.html')
 
-#Envio de datos a través de JSON
+#Envio de datos a traves de JSON
 @servidorWeb.route('/modelo',methods=['POST'])
 def modeloPrediccion():
 	#Procesar los datos de entrada
 	contenido =request.json
 	print(contenido)
+	datosEntrada=np.array([
+		contenido['pH'],
+		contenido['sulfatos'],
+		contenido['alcohol']
+	])
 	return jsonify({'resultado':'Hola'})
 	
 
-if __name__ == '__main__':
-    servidorWeb.run(debug=False,host='0.0.0.0',port='8080')
-
+if _name_ == '_main_':
+	servidorWeb.run(debug=False,host='0.0.0.0',port='8080')
