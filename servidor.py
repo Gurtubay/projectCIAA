104d8dd6573f2189830468995f990efc128d8528
José Miguel García Gurtubay Moreno <miguelgarcia@ezigna.com>
2023-09-06 19:21:42 -0600

    ERROR __MAIN__

--- servidor.py
+++ servidor.py
@@ -29,5 +29,5 @@ def modeloPrediccion():
 	return jsonify({'resultado':str(resultado[0])})
 
 
-if __name__ == '_main_':
+if __name__ == '__main__':
 	servidorWeb.run(debug=False,host='0.0.0.0',port='8080')
