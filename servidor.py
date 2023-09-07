642e3879b5cbcac3f7607ef89c964c83018d9d45
EC2 Default User <ec2-user@ip-172-31-52-39.ec2.internal>
2023-09-07 00:56:33 +0000

    repo proge

--- servidor.py
+++ servidor.py
@@ -1,4 +1,4 @@
-from flask import Flask, render_template, request, jsonify
+from flask import Flask, render_template,jsonify,request
 import numpy as np
 from joblib import load
 import os
@@ -6,22 +6,22 @@ import os
 #Cargar el modelo
 dt=load('dt1.joblib')
 
-#Generar el servidor(Back-End)
-servidorWeb=Flask(__name__)
+#Servidor (backend)
+servidorWeb = Flask(__name__)
 
 @servidorWeb.route("/holamundo",methods=['GET'])
 def formulario():
-	return render_template('pagina1.html')
+    return render_template('pagina1.html')
 
-#Envio de datos a traves de JSON
+#Envio de datos a través de JSON
 @servidorWeb.route('/modelo',methods=['POST'])
 def modeloPrediccion():
-	#Procesar los datos de entrada
-	contenido =request.json
-	print(contenido)
-	return jsonify({'resultado':'Hola'})
-	
+    #Procesar los datos de entrada
+    contenido = request.json
+    print(contenido)
+    return jsonify({'resultado':"Hola"})
+
 
 if __name__ == '__main__':
-	servidorWeb.run(debug=False,host='0.0.0.0',port='8080')
+    servidorWeb.run(debug=False,host='0.0.0.0',port='8080')
 
